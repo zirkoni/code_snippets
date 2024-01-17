5dacf9910bbb4e3c640751a438501580707e6900
orantala <olli.rantala@nokia.com>
2024-01-17 10:49:35 +0200

    Updated way to print

--- chrono_time_stuff.cpp
+++ chrono_time_stuff.cpp
@@ -10,12 +10,12 @@ int main()
     auto   tnow = std::chrono::system_clock::now();
     time_t tnowTime = std::chrono::system_clock::to_time_t(tnow);
     tm*    tnowDate = std::localtime(&tnowTime);
-    
+
     // 1 minute from now
     uint16_t delayDays = 0;
     uint16_t minutesFromMidnight = tnowDate->tm_hour * 60 + tnowDate->tm_min + 1;
     uint16_t secondsFromMidnight = tnowDate->tm_sec;
-    
+
     SetActionTime(delayDays, minutesFromMidnight, secondsFromMidnight);
 
     return 0;
@@ -25,22 +25,27 @@ void SetActionTime(uint16_t delayDays, uint16_t minutesFromMidnight,uint16_t sec
 {
     std::cout << "set action time delayDays:" << delayDays << ", minutesFromMidnight:" << minutesFromMidnight
               << ", secondsFromMidnight:" << secondsFromMidnight << std::endl;
-    
+
     std::chrono::system_clock::duration actionTime = calculateDelay(delayDays, minutesFromMidnight, secondsFromMidnight);
     auto actionTimeout = std::chrono::duration_cast<std::chrono::seconds>(actionTime).count();
-    
+
     std::cout << "\nactionTimeout:" << (uint64_t)actionTimeout << std::endl;
 }
 
 std::chrono::system_clock::duration calculateDelay(uint16_t delayDays, uint16_t minutesFromMidnight, uint16_t secondsFromMidnight)
 {
     auto   tnow               = std::chrono::system_clock::now();
-    
-    std::time_t tnowTemp = std::chrono::system_clock::to_time_t(tnow);
+
+    // Print
+    /*std::time_t tnowTemp = std::chrono::system_clock::to_time_t(tnow);
     std::tm tmNow = *std::localtime(&tnowTemp);
-    std::cout << "Date now:     " << std::put_time(&tmNow, "%c %Z") << std::endl;
-    
-    
+    std::cout << "Date now:     " << std::put_time(&tmNow, "%c %Z") << std::endl;*/
+
+    // Simpler print
+    const std::time_t tPrint = std::chrono::system_clock::to_time_t(tnow);
+    std::cout << "Date now:     " << std::ctime(&tPrint);
+
+
     auto   triggerDay         = tnow + std::chrono::days(delayDays);
     time_t triggerDayAsTime = std::chrono::system_clock::to_time_t(triggerDay);
     tm*    date               = std::localtime(&triggerDayAsTime);
@@ -53,10 +58,9 @@ std::chrono::system_clock::duration calculateDelay(uint16_t delayDays, uint16_t 
     {
         triggerDateAndHour = triggerDateAndHour + std::chrono::days(1);
     }
-    
-    std::time_t tmp = std::chrono::system_clock::to_time_t(triggerDateAndHour);
-    std::tm tmptr = *std::localtime(&tmp);
-    std::cout << "Trigger date: " << std::put_time(&tmptr, "%c %Z") << std::endl;
+
+    const std::time_t tPrint2 = std::chrono::system_clock::to_time_t(triggerDateAndHour);
+    std::cout << "Date now:     " << std::ctime(&tPrint2);
 
     return triggerDateAndHour - tnow;
 }
